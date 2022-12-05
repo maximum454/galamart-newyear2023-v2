d3efaffa36ac6c2bf7a218598e9960f22fa20ddc
Maxim Karachyov <karachyov.m@gmail.com>
2022-12-05 13:41:44 +0300

    fix

--- src/js/newyear2023.js
+++ src/js/newyear2023.js
@@ -2,18 +2,14 @@
 @@include('./partials/swiper-bundle.min.js')
 @@include('./partials/jquery.countdown.min.js')
 
+$(function (){
+    $('#clock').countdown('2023/01/01 00:00', function(event) {
+        $(this).html(event.strftime('<span class="time">%D <span class="text">дней</span></span>' +
+            '<span class="time">%H <span class="text">часов</span></span>' +
+            '<span class="time">%M <span class="text">минут</span></span>'));
+    });
+})
 
-$('#getting-started').countdown('2022/12/31', function(event) {
-    $(this).html(event.strftime('' +
-        '<span class="time">%d <span class="text">дней</span></span>' +
-        '<span class="time">%H <span class="text">часов</span></span>' +
-        '<span class="time">%M <span class="text">минут</span></span>'
-    ));
-}).on('finish.countdown', function(event) {
-    $(this).html('С новым годом')
-        .parent().addClass('disabled');
-
-});
 
 setTimeout(function() {
     $('.js-select').styler();
@@ -32,9 +28,13 @@ const swiper = new Swiper('.swiper-block-item', {
             spaceBetween: 20
         },
         480: {
-            slidesPerView: 4,
+            slidesPerView: 3,
         },
         768: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+        1024: {
             slidesPerView: 5,
             spaceBetween: 30,
         }
