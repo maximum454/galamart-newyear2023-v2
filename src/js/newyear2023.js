3d81c75671b285b449138ded2a9107ba292a4fef
Maxim Karachyov <karachyov.m@gmail.com>
2022-12-05 09:57:43 +0300

    project

--- src/js/newyear2023.js
+++ src/js/newyear2023.js
@@ -1,5 +1,28 @@
 @@include('./partials/jquery.formstyler.js')
+@@include('./partials/swiper-bundle.min.js')
 
 setTimeout(function() {
     $('.js-select').styler();
 }, 100)
+
+const swiper = new Swiper('.swiper-block-item', {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        480: {
+            slidesPerView: 4,
+        },
+        768: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+        }
+    }
+})
